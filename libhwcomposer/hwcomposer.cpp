30cb87734f04ece84e71efaac9ea5a435b4812ae
Jacob Bell <jacob@fzservers.com>
2012-02-08 15:15:01 -0500

    libcopybit: Disable copybit blending for opaque layers
    This change disables copybit blending when surface flinger sets
    HWC_BLENDING_NONE for a layer.
    
    Change-Id: Ie86a2d5c9e3d3dab1bc7a0a3cc31bff7afe604da
    
    Conflicts:
    
    	libhwcomposer/hwcomposer.cpp

--- libhwcomposer/hwcomposer.cpp
+++ libhwcomposer/hwcomposer.cpp
@@ -1372,7 +1372,7 @@ static int drawLayerUsingCopybit(hwc_composer_device_t *dev, hwc_layer_t *layer,
     copybit->set_parameter(copybit, COPYBIT_FRAMEBUFFER_HEIGHT, renderBuffer->height);
     copybit->set_parameter(copybit, COPYBIT_TRANSFORM, layer->transform);
     copybit->set_parameter(copybit, COPYBIT_PLANE_ALPHA,
-                           (layer->blending == HWC_BLENDING_NONE) ? -1 : layer->alpha);
+                           (layer->blending == HWC_BLENDING_NONE) ? 0x0 : layer->alpha);
     copybit->set_parameter(copybit, COPYBIT_PREMULTIPLIED_ALPHA,
                            (layer->blending == HWC_BLENDING_PREMULT)? COPYBIT_ENABLE : COPYBIT_DISABLE);
     copybit->set_parameter(copybit, COPYBIT_DITHER,
