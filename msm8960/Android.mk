b5729b7ece822fce79a0a1b449d1967abaf4ee93
bmc08gt <brandon.mcansh@gmail.com>
2013-11-09 18:11:09 +0100

    Adopt Google's device specific conditional nomenclature
    * TARGET_PROVIDES_* -> USE_DEVICE_SPECIFIC_*
    
    Signed-off-by: bmc08gt <brandon.mcansh@gmail.com>

--- msm8960/Android.mk
+++ msm8960/Android.mk
@@ -1,7 +1,7 @@
 display-hals := libgralloc libgenlock libcopybit
 display-hals += libhwcomposer liboverlay libqdutils libexternal libqservice
 display-hals += libmemtrack
-ifneq ($(TARGET_PROVIDES_LIBLIGHT),true)
+ifneq ($(USE_DEVICE_SPECIFIC_LIBLIGHT),true)
 display-hals += liblight
 endif
 
